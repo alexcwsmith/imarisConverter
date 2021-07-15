8d08c7de3eeb741a9c83458777da7c6aa5389386
Alex Smith <alexander.smith@mssm.edu>
2021-07-15 13:47:33 -0400

    Minor updates

--- ACWS_extractImaris.py
+++ ACWS_extractImaris.py
@@ -21,12 +21,15 @@ def _multiSaveIMS(plane, imsFile, channel):
     n, e = os.path.splitext(name)
     imNumPadded = str(plane).zfill(4)
     directory = os.path.dirname(imsFile)
-    if not os.path.exists(os.path.join(directory, 'FullResImages_C' + str(channel)+'/')):
-        os.mkdir(os.path.join(directory, 'FullResImages_C' + str(channel)+'/'))
     data = file.get('DataSet/ResolutionLevel 0/TimePoint 0/Channel ' + str(channel)+'/Data')
-    imsave(os.path.join(directory, 'FullResImages_C'+str(channel)+'/' + n + '_C' + str(channel) + '_Z' + imNumPadded + '.tif'), np.array(data[plane]), check_contrast=False)
+    imsave(os.path.join(directory, n + '_C'+str(channel)+'/' + n + '_C' + str(channel) + '_Z' + imNumPadded + '.tif'), np.array(data[plane]), check_contrast=False)
   
 def multiSaveIMS(imsFile, channel, nthreads):
+    name = os.path.basename(imsFile)
+    n, e = os.path.splitext(name)
+    directory = os.path.dirname(imsFile)
+    if not os.path.exists(os.path.join(directory, n + '_C' + str(channel)+'/')):
+        os.mkdir(os.path.join(directory, n + '_C' + str(channel)+'/'))
     print("Started at " + time.ctime())
     file = h5py.File(imsFile, 'r')
     data = file.get('DataSet/ResolutionLevel 0/TimePoint 0/Channel ' + str(channel) + '/Data')
